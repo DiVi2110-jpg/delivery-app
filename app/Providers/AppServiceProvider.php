a482beedc5f081eb7788a3e1d6860a2c7830adb5
DiVi2110-jpg <daniil.virabyan@gmail.com>
2026-03-06 16:39:39 +0300

    Force https in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Filament\Support\Facades\FilamentView;
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,11 +16,15 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         FilamentView::registerRenderHook(
             'panels::body.end',
             function () {
                 // ✅ Никогда не ломаем админку, даже если файла нет
-                if (! View::exists('filament.new-order-sound')) {
+                if (!View::exists('filament.new-order-sound')) {
                     return '';
                 }
 
